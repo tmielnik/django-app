9d3d01dbc1192a29d45ca377ea9ab21238f4bac0
TiagoAM <tiago@teste.com>
2022-06-18 22:21:05 -0400

    Mudado banco de dados para postgres

--- main/settings.py
+++ main/settings.py
@@ -77,9 +77,13 @@ WSGI_APPLICATION = 'main.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": config('NAME'),
+        "USER": config('USER'),
+        "PASSWORD": config('PASSWORD'),
+        "HOST": config('DB_HOST'),
+        "PORT": config('PORT'),
     }
 }
 
