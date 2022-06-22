1b23bc5a69b6633e7c0a8a6fbe7c58f15f8a7ca6
Tiago AM <tiagoamielnik@gmail.com>
2022-06-22 19:52:32 -0400

    Ajuste de view para tipo generico

--- home/views.py
+++ home/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
-def index(request):
-    return render(request, 'home/index.html')
+class HomeView(TemplateView):
+    template_name = 'home/index.html'
