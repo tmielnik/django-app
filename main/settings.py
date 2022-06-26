100b8d272614feb6687b2f4a98973158821159e7
TiagoAM <tiago@teste.com>
2022-06-25 22:47:43 -0400

    Ajustado variáveis de ambiente em databases

--- main/settings.py
+++ main/settings.py
@@ -79,11 +79,11 @@ WSGI_APPLICATION = 'main.wsgi.application'
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": config('NAME'),
-        "USER": config('USER'),
-        "PASSWORD": config('PASSWORD'),
-        "HOST": config('DB_HOST'),
-        "PORT": config('PORT'),
+        "NAME": config('POSTGRES_NAME'),
+        "USER": config('POSTGRES_USER'),
+        "PASSWORD": config('POSTGRES_PASSWORD'),
+        "HOST": config('POSTGRES_DB'),
+        "PORT": config('POSTGRES_PORT'),
     }
 }
 
