d1717199dc8b6d3f93f643f4c15c9b14a282c0ce
TiagoAM <tiago@teste.com>
2022-07-02 01:05:23 -0400

    Ajustado campos do modelo Agendamento

--- agendamento/models.py
+++ agendamento/models.py
@@ -7,7 +7,8 @@ class Agendamento(models.Model):
     usuario = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     descricao = models.CharField(max_length=100)
     data_registro = models.DateTimeField(default=timezone.now)
-    data_agendada = models.DateTimeField(blank=True, null=True)
+    data_agendada = models.DateField(blank=True, null=True)
+    hora_agendada = models.TimeField(blank=True, null=True)
 
     def get_user(self):
         return self.usuario
