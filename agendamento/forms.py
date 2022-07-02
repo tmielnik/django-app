4595933cecc6e36945d35f1c4dbf119707280376
TiagoAM <tiago@teste.com>
2022-07-02 01:04:51 -0400

    Personalizado campos do form

--- agendamento/forms.py
+++ agendamento/forms.py
@@ -4,13 +4,38 @@ from agendamento.models import Agendamento
 class AgendamentoForm(forms.ModelForm):
     class Meta:
         model = Agendamento
-        exclude = ('data_registro',)
+        exclude = ("data_registro", "usuario")
         
-        widgets = {
-            'descricao': forms.TextInput(
-                attrs={'class':'form-control'}
-            ),
-            'data_agendament': forms.DateTimeInput(
-                attrs={'class': 'form-control', "type": "date"}
-            )
-        }
+    descricao = forms.CharField(
+        required=True,
+        max_length=50,
+        label="Descrição",
+        widget=forms.TextInput(
+            attrs={
+                "class": "form-control",
+            }
+        )
+    )
+
+    data_agendada = forms.DateField(
+        required=True,
+        label="Data",
+        widget=forms.DateInput(
+            format='%d-%m-%Y',
+            attrs={
+                "class": "form-control",
+                "type": "date"
+            }
+        )
+    )
+
+    hora_agendada = forms.TimeField(
+        required=True,
+        label="Hora",
+        widget=forms.TimeInput(
+            attrs={
+                "class": "form-control",
+                "type": "time"
+            }
+        )
+    )
