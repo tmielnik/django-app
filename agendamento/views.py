b14c59706378eaaea0323d62a21f5ab8cc052b4e
TiagoAM <tiago@teste.com>
2022-07-02 01:06:05 -0400

    Ajustado organização das views

--- agendamento/views.py
+++ agendamento/views.py
@@ -2,10 +2,11 @@ from django.shortcuts import redirect, render
 from agendamento.forms import AgendamentoForm
 from .models import Agendamento
 
-
+# Pagina inicial de agendamentos
 def index_agendamento(request):
     return render(request, "agendamento/index_agendamento.html", {})
 
+# Lista agendamentos realizados
 def lista_agendamentos(request):
     agendamentos = Agendamento.objects.all()
     context = {
@@ -13,12 +14,15 @@ def lista_agendamentos(request):
     }
     return render(request, "agendamento/list.html", context)
 
+# Adiciona novos agendamentos
 def add_agendamento(request):
     form = AgendamentoForm(request.POST or None)
     
     if request.method == "POST":
         if form.is_valid():
-            form.save()
+            obj = form.save(commit=False)
+            obj.usuario = request.user
+            obj.save()
             return redirect("lista_agendamentos")
     
     context = {
