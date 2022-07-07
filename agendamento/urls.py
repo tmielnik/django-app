1c5de008437d9fcb23db67b4ba65d1812de77dd6
Tiago AM <tiagoamielnik@gmail.com>
2022-07-06 20:01:20 -0400

    Incluído url para view delete

--- agendamento/urls.py
+++ agendamento/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.index_agendamento, name='index_agendamento'),
     path('list/', views.lista_agendamentos, name='lista_agendamentos'),
     path('add/', views.add_agendamento, name='add_agendamento'),
-    path('edit/<int:agendamento_pk>', views.edit_agendamento, name='edit_agendamento')
+    path('edit/<int:pk_agendamento>', views.edit_agendamento, name='edit_agendamento'),
+    path('delete/<int:pk_agendamento>', views.delete_agendamento, name='delete_agendamento')
 ]
